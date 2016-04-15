cf51bf5fe81a0aa53f5b66379c440cea91e8203f
homeu98 <homeu98@hotmail.com>
2016-04-16 01:04:47 +0800

    Added fireFox inside
    The firefox will shoot at the player.

---
+++ BouncyGame/Assets/Enemies/fireFox/bulletScript.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bulletScript : MonoBehaviour {
+
+	GameObject thePlayer;
+	public float speed = 1.0f;
+
+
+
+	// Use this for initialization
+	void Start () {
+
+
+		thePlayer = GameObject.FindWithTag ("Player");
+		transform.LookAt (thePlayer.transform);
+
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		transform.Translate (Vector3.forward * speed * Time.deltaTime);
+
+	}
+
+	void OnCollisionEnter(Collision other){
+
+		//no mater what the fireball hits, it will just explode.
+	
+		//the bullet hits the player, it dies.
+
+		other.gameObject.SendMessage ("die", null, SendMessageOptions.DontRequireReceiver);
+
+		//In here, we can add explosion to the bullet when it hits the player.
+
+		Destroy (this.gameObject);
+	}
+
+}
---
+++ BouncyGame/Assets/Enemies/fireFox/fireFoxAttack.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fireFoxAttack : MonoBehaviour {
+
+	public GameObject fireBall;
+	float playerLocation;
+	float shootPeroid = 3.0f;
+	GameObject player;
+
+	// Use this for initialization
+	void Start () {
+
+		player = GameObject.FindWithTag ("Player");
+		InvokeRepeating ("attack", 2.0f, shootPeroid);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	
+	}
+
+	void attack(){
+
+		/*
+		 * 
+		 * simple attack, here can add attack animation 
+
+		*/
+
+		Vector3 playerPosition = player.transform.position;
+
+		Vector3 currentLocation = new Vector3 (this.transform.position.x, this.transform.position.y, this.transform.position.z - 1.0f);
+
+		Instantiate (fireBall, currentLocation, this.transform.localRotation);
+
+	
+	}
+
+}
