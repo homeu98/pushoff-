67d4f8687fb6114fc0c0845eb6ec444d87f4bf38
homeu98 <homeu98@hotmail.com>
2016-04-25 01:52:50 +0800

    cow boy & crazy chicken
    Added cowboy inside, is still a bit buggy.
    
    Crazy chicken prefab is in now.

---
+++ BouncyGame/Assets/Enemies/cowBoy/cowBoyBullet.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cowBoyBullet : MonoBehaviour {
+
+	public float bulletSpeed;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		this.transform.Translate (Vector3.forward * bulletSpeed * Time.deltaTime);
+	
+	}
+}
