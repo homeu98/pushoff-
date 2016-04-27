681a02dc59fc7f116c0a3702511b1529fd037926
nickyMouse2 <dick28439@hotmail.com>
2016-04-27 21:07:18 +0800

    Change SpawnScript(uncompleted)
    Change SpawnScript(uncompleted)

--- BouncyGame/Assets/MultidimensionalArray.cs
+++ BouncyGame/Assets/MultidimensionalArray.cs
@@ -3,15 +3,19 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class MultidimensionalArray : MonoBehaviour {
+	public int NumberOfEnemy1 = 5;
+	public List<GameObject > Group1 = new List<GameObject>();
+	public List<Transform> Group1SpawnPoint = new List<Transform>();
 
-	public List<GameObject > Group = new List<GameObject>();
-	public List<Transform> EnemySpawnPoint = new List<Transform>();
+	public int NumberOfEnemy2 = 5;
+	public List<GameObject > Group2 = new List<GameObject>();
+	public List<Transform> Group2SpawnPoint = new List<Transform>();
 	// Use this for initialization
 	void Start () {
-		for (int i = 0 ; i < Group.Count ; i++){
-			int SortOfEnemyID = Random.Range (0, Group.Count);
-			int SpawnPosID = Random.Range (0, EnemySpawnPoint.Count);
-			Instantiate (Group[SortOfEnemyID],  EnemySpawnPoint[SpawnPosID].position, Quaternion.identity);
+		for (int i = 0 ; i < NumberOfEnemy1 ; i++){
+			int SortOfEnemyID = Random.Range (0, Group1.Count);
+			int SpawnPosID = Random.Range (0, Group1SpawnPoint.Count);
+			Instantiate (Group1[SortOfEnemyID],  Group1SpawnPoint[SpawnPosID].position, Group1[SortOfEnemyID].transform.rotation);
 		}
 	}
 	
